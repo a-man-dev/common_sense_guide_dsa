26443843d95aeaa87be9dc93ab3c7d1fafc2daaf
Aman <a.shalginbayev@yandex.com>
2026-03-03 13:54:32 +0500

    Chapter19 ex

---
+++ Chapter19/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+Exercise solutions:
+19.1
+For the array size of n elements, new collections array created with the size of n^2, so space complexity is O(n^2)
+
+19.2
+Its space complexity is O(n).
+*/
+
+string[] Reverse(string[] input) {
+	
+}
