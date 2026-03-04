5ceebe951dd5ab142e99453bad2e85bfc523b613
Aman <a.shalginbayev@yandex.com>
2026-03-04 13:56:07 +0500

    Chapter19

--- Chapter19/Program.cs
+++ Chapter19/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Common;
 
 /*
 Exercise solutions:
@@ -10,6 +9,23 @@ For the array size of n elements, new collections array created with the size of
 Its space complexity is O(n).
 */
 
+// 19.3
 string[] Reverse(string[] input) {
-	
+	for (var i = 0; i < input.Length /2; i++) {
+		var temp = input[i];
+		input[i] = input[input.Length - 1 - i];
+		input[input.Length - 1 - i] = temp;
+	}
+	return input;
 }
+
+var words = new [] {"a", "b", "c", "d", "e"};
+Reverse(words).PrintElements();
+
+// 19.4
+/*
+Version   | Time Complexity   | Space Complexity (How much extra space allocated)
+#1			 	O(n)		    	O(n)
+#2				O(n)				O(1)
+#3				
+*/
